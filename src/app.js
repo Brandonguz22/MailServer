f5aaae1507cd2b6338ca46c39bcc59b103809172
Bguz <ballouboyjguz22@gmail.com>
2023-05-17 09:33:03 -0400

    Correccion de los scripts del proyecto en el package.json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const router = require('./routes/mailSender');
+const cors = require("cors");
 const app = express();
+
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use(cors());
+app.use("/api/mail", router);
+
+app.get("/",(req, res)=>{
+    res.send("Pagina Principal");
+})
 
 app.listen(port, ()=>{
     console.log("Servidor corriendo en el puerto",port)
